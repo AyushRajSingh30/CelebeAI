f527b6fe7de505fedcbbb9f5b090346711e03da8
Ayush Raj Singh <ayushrajsin30@gmail.com>
2025-02-22 23:46:44 +0530

    Login Page

---
+++ app/profile/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Profile() {
+
+
+  return (
+    <>
+    {/* {} */}
+    <div>
+    <div>Profile Image</div>
+    {/* store in state and coming from data base */}
+    <div>Profile Name</div>
+     </div>
+    
+
+    </>
+  )
+}
+
+export default page
